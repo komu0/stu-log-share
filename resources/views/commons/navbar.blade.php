7bd893e37b336c5e49d927d78054cdec64de0528
komu <komu.engineer@gmail.com>
2020-07-28 11:26:41 +0000

    fix navbar for login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,6 +8,19 @@
         </button>
 
         <div class="collapse navbar-collapse" id="nav-bar">
+            @if (Auth::check())
+            <li class="nav-item dropdown">
+                <li class="nav-item"><a href="#" class="nav-link">ログを投稿</a></li>
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        {{-- ユーザ詳細ページへのリンク --}}
+                        <li class="dropdown-item"><a href="#">マイページ</a></li>
+                        <li class="dropdown-divider"></li>
+                        {{-- ログアウトへのリンク --}}
+                        <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+                    </ul>
+                </li>
+            @else
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 <li class="nav-item"><a href="#" class="nav-link">スタログシェアとは？</a></li>
