277d9992530b8eb361535e5d0e3d1f0e1b43cbde
komu <komu.engineer@gmail.com>
2020-08-02 13:37:52 +0000

    ユーザページにフォロー・フォロワーページへのリンクを追加。

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,8 +6,8 @@
     <p>開始日:{{ $user->created_at->format('Y年m月d日') }}</p>
     <p>総勉強時間:{{ $user->studyTime() }}</p>
     <p>{{ $user->profile }}</p>
-    <p>フォロー:{{ $user->followings_count }}</p>
-    <p>フォロワー:{{ $user->followers_count }}</p>
+    <p>フォロー:{!! link_to_route('users.followings', $user->followings_count , ['id' => $user->id]) !!}</p>
+    <p>フォロワー:{!! link_to_route('users.followers', $user->followers_count , ['id' => $user->id]) !!}</p>
     <h2>ログ</h2>
     @include('stulogs.stulogs')
 @endsection
