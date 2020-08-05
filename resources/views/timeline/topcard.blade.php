167a27d51504fb27a0eb6ecee4774e54252e02fd
komu <komu.engineer@gmail.com>
2020-08-05 00:43:09 +0000

    topcardにユーザ詳細ページ・フォロー・フォロワーページへのリンクを追加。

--- resources/views/timeline/topcard.blade.php
+++ resources/views/timeline/topcard.blade.php
@@ -1,8 +1,8 @@
 <div class="mb-4">
-  <p>{{ $user->id }}</p>
-  <p>followings:{{ $user->followings_count }}</p>
-  <p>followers:{{ $user->followers_count }}</p>
-  <a href="{{ route('stulogs.create') }}" class="btn btn-primary">ログを投稿</a>
+    <p>{!! link_to_route('users.show', $user->id, ['user' => $user->id]) !!}</p>
+    <p>フォロー:{!! link_to_route('users.followings', $user->followings_count , ['id' => $user->id]) !!}</p>
+    <p>フォロワー:{!! link_to_route('users.followers', $user->followers_count , ['id' => $user->id]) !!}</p>
+    <a href="{{ route('stulogs.create') }}" class="btn btn-primary">ログを投稿</a>
 </div>
 <ul class="nav nav-tabs mb-5">
     <li class="nav-item">
