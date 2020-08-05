456674137ca9a66fe6cd0032cf3a6bad5bb455c0
komu <komu.engineer@gmail.com>
2020-08-05 10:20:13 +0000

    TimelineControllerを修正。

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -9,28 +9,13 @@ class TimelineController extends Controller
 {
     public function index()
     {
-        if (\Auth::check()) {
-            $user = \Auth::user();
-            $user->loadRelationshipCounts();
-            $stulogs = $user->feed_stulogs()->orderBy('log_date', 'desc')->orderBy('updated_at', 'desc')->paginate(10);
-        }
+        $user = \Auth::user();
+        $user->loadRelationshipCounts();
+        $stulogs = $user->feed_stulogs()->orderBy('log_date', 'desc')->orderBy('updated_at', 'desc')->paginate(10);
         
         return view('timeline.index', [
             'stulogs' => $stulogs,
             'user' => $user,
         ]);
-        
-        
-        // if (\Auth::check()) { // 認証済みの場合
-        //     // 認証済みユーザを取得
-        //     $user = \Auth::user();
-        //     // ユーザの投稿の一覧を作成日時の降順で取得
-        //     $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
-
-        //     $data = [
-        //         'user' => $user,
-        //         'microposts' => $microposts,
-        //     ];
-        //}
     }
 }
