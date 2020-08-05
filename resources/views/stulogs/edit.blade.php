37ff15dcddfc7fd5c6f76f658ec09c1d309b4ac4
komu <komu.engineer@gmail.com>
2020-08-05 10:52:36 +0000

    stulogs.editのインデントを修正。

--- resources/views/stulogs/edit.blade.php
+++ resources/views/stulogs/edit.blade.php
@@ -1,37 +1,36 @@
 @extends('layouts.app')
 @section('content')
 <h1>ログを編集</h1>
+<div class="row">
+    <div class="col-6">
+        {!! Form::model($stulog, ['route' => ['stulogs.update', $stulog->id], 'method' => 'put']) !!}
+            <div class="form-group">
+                {!! Form::label('log_date', '日付:') !!}
+                {!! Form::date('log_date', $stulog->log_date, ['class' => 'form-control']) !!}
+            </div>
+            
+            <div class="form-group">
+                {!! Form::label('study_time', '勉強時間:') !!}
+                {!! Form::time('study_time', $study_time, ['class' => 'form-control']) !!}
+            </div>
+            
+            <div class="form-group">
+                {!! Form::label('content', '内容:') !!}
+                {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
+            </div>
 
-    <div class="row">
-        <div class="col-6">
-            {!! Form::model($stulog, ['route' => ['stulogs.update', $stulog->id], 'method' => 'put']) !!}
-                <div class="form-group">
-                    {!! Form::label('log_date', '日付:') !!}
-                    {!! Form::date('log_date', $stulog->log_date, ['class' => 'form-control']) !!}
-                </div>
-                
-                <div class="form-group">
-                    {!! Form::label('study_time', '勉強時間:') !!}
-                    {!! Form::time('study_time', $study_time, ['class' => 'form-control']) !!}
-                </div>
-                
-                <div class="form-group">
-                    {!! Form::label('content', '内容:') !!}
-                    {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
-                </div>
+            <div class="form-group">
+                {!! Form::label('thought', '感想:') !!}
+                {!! Form::textarea('thought', null, ['class' => 'form-control']) !!}
+            </div>
+            
+            {!! Form::submit('編集', ['class' => 'btn btn-primary']) !!}
 
-                <div class="form-group">
-                    {!! Form::label('thought', '感想:') !!}
-                    {!! Form::textarea('thought', null, ['class' => 'form-control']) !!}
-                </div>
-                
-                {!! Form::submit('編集', ['class' => 'btn btn-primary']) !!}
-
-            {!! Form::close() !!}
-            {{-- 投稿削除ボタンのフォーム --}}
-            {!! Form::open(['route' => ['stulogs.destroy', $stulog->id], 'method' => 'delete']) !!}
-                {!! Form::submit('この投稿を削除', ['class' => 'btn btn-danger btn-sm']) !!}
-            {!! Form::close() !!}
-        </div>
+        {!! Form::close() !!}
+        {{-- 投稿削除ボタンのフォーム --}}
+        {!! Form::open(['route' => ['stulogs.destroy', $stulog->id], 'method' => 'delete']) !!}
+            {!! Form::submit('この投稿を削除', ['class' => 'btn btn-danger btn-sm']) !!}
+        {!! Form::close() !!}
     </div>
+</div>
 @endsection
