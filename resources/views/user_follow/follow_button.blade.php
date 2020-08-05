2474a5f79ef23c749179bb75dbec8ce47a8cced6
komu <komu.engineer@gmail.com>
2020-08-05 02:31:19 +0000

    users.showのフォローボタンのみにmb-4追加。

--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -1,14 +1,14 @@
-@if (Auth::check())
+ @if (Auth::check())
     @if (Auth::id() != $user->id)
         @if (Auth::user()->is_following($user->id))
             {{-- アンフォローボタンのフォーム --}}
             {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
-                {!! Form::submit('フォロー中', ['class' => "btn btn-primary mb-4"]) !!}
+                {!! Form::submit('フォロー中', ['class' => "btn-primary btn"]) !!}
             {!! Form::close() !!}
         @else
             {{-- フォローボタンのフォーム --}}
             {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
-                {!! Form::submit('フォロー', ['class' => "btn btn-light mb-4"]) !!}
+                {!! Form::submit('フォロー', ['class' => "btn btn-light"]) !!}
             {!! Form::close() !!}
         @endif
     @endif
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,7 +8,9 @@
     <p>{{ $user->profile }}</p>
     <p>フォロー:{!! link_to_route('users.followings', $user->followings_count , ['id' => $user->id]) !!}</p>
     <p>フォロワー:{!! link_to_route('users.followers', $user->followers_count , ['id' => $user->id]) !!}</p>
-    @include('user_follow.follow_button')
+    <div class=mb-4>
+        @include('user_follow.follow_button')
+    </div>
     <h2>ログ</h2>
     @include('stulogs.stulogs')
 @endsection
