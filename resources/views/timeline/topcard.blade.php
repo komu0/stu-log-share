eb7a9d0131d0c751a307b65c39c90e58f123460b
komu <komu.engineer@gmail.com>
2020-08-06 14:13:18 +0000

    topcardを編集。

--- resources/views/timeline/topcard.blade.php
+++ resources/views/timeline/topcard.blade.php
@@ -1,8 +1,12 @@
 <div class="mb-4">
-    <p>{!! link_to_route('users.show', $user->id, ['user' => $user->id]) !!}</p>
-    <p>フォロー:{!! link_to_route('users.followings', $user->followings_count , ['id' => $user->id]) !!}</p>
-    <p>フォロワー:{!! link_to_route('users.followers', $user->followers_count , ['id' => $user->id]) !!}</p>
-    <a href="{{ route('stulogs.create') }}" class="btn btn-primary">ログを投稿</a>
+    <div>
+        <h2>{!! link_to_route('users.show', $user->id, ['user' => $user->id]) !!}さん、こんにちは！</h2>
+        <p><span>フォロー:{!! link_to_route('users.followings', $user->followings_count , ['id' => $user->id]) !!} / </span>
+        <span>フォロワー:{!! link_to_route('users.followers', $user->followers_count , ['id' => $user->id]) !!}</span></p>
+    </div>
+    <div>
+        <a href="{{ route('stulogs.create') }}" class="btn btn-primary">ログを投稿</a>
+    </div>
 </div>
 <ul class="nav nav-tabs mb-5">
     <li class="nav-item">
