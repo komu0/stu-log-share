90a3545ab0a3b6caa160b318bb03d5ef43096851
komu <komu.engineer@gmail.com>
2020-08-08 02:09:26 +0000

    404notfoundページを追加。

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+    <div class="text-center">
+        <h1 class='display-3 mb-5'>404 NOT FOUND</h1>
+        <p class='h2 mb-5'>存在しないページです。</p>
+        <a href='/'>ホームへ戻る</a>
+    </div>
+@endsection
