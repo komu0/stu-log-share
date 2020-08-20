89ec0cc4560e4dfa4a5d2257d2588bddadeab5d9
komu <komu.engineer@gmail.com>
2020-08-20 11:03:09 +0000

    カテゴリ・タグ追加のモーダルを修正。

--- resources/views/modal/add_tag.blade.php
+++ resources/views/modal/add_tag.blade.php
@@ -1,26 +1,26 @@
-<div class="modal" id="addTagOn{{$i}}" tabindex="-1" role="dialog" aria-hidden="true">
-     <div class="modal-dialog" role="document">
+<div class="modal" id="addTagOn{{$i}}" tabindex="-1" aria-hidden="true">
+     <div class="modal-dialog-centered modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">「{{$category->name}}」へのタグの追加</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <h5 class="modal-title">カテゴリ「{{$category->name}}」にタグを追加する</h5>
+                <button type="button" class="close" data-dismiss="modal">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body ml-2">
+            <div class="modal-body ml-3">
                 {!! Form::model($category->tag, ['route' => ['tags.store', $category->id]]) !!}
                     <div class="row">
-                        <div class="form-group d-flex align-items-end mr-2">
+                        <div class="d-flex align-items-end mr-2">
                             {!! Form::label('name', 'タグ名:') !!}
                         </div>
-                        <div class="form-group">
+                        <div>
                             {!! Form::text('name', null, ['class' => 'form-control']) !!}
                         </div>
                     </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">キャンセル</button>
-                {!! Form::submit('追加', ['class' => 'btn btn-primary btn-sm']) !!}
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
+                {!! Form::submit('追加', ['class' => 'btn btn-primary']) !!}
                 {!! Form::close() !!}
             </div>
         </div>
