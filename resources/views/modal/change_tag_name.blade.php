090963398a9d585ecbc572e5fde1c000a018980e
komu <komu.engineer@gmail.com>
2020-08-20 13:28:06 +0000

    タグ名変更のモーダルを修正。

--- resources/views/modal/change_tag_name.blade.php
+++ resources/views/modal/change_tag_name.blade.php
@@ -1,23 +1,26 @@
-<div class="modal" id="changeTagName{{$i}}_{{$j}}" tabindex="-1" role="dialog" aria-hidden="true">
-     <div class="modal-dialog" role="document">
+<div class="modal" id="changeTagName{{$i}}_{{$j}}" tabindex="-1" aria-hidden="true">
+     <div class="modal-dialog-centered modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">タグ名の変更</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <h5 class="modal-title">タグ名を変更する</h5>
+                <button type="button" class="close" data-dismiss="modal">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body ml-2">
-                {!! Form::open( ['route' => ['update.tag.name', $tag->id], 'method' => 'put']) !!}
-                <div class="row">
-                    <div class="form-group">
-                        {!! Form::text("name", $tag->name, ['class' => 'form-control']) !!}
+                {{$tag->name}}→
+                <div class="ml-3">
+                    {!! Form::open( ['route' => ['update.tag.name', $tag->id], 'method' => 'put']) !!}
+                    <div class="row">
+                        <div>
+                            {!! Form::text("name", $tag->name, ['class' => 'form-control']) !!}
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">キャンセル</button>
-                    {!! Form::submit('変更', ['class' => 'btn btn-primary btn-sm']) !!}
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
+                    {!! Form::submit('変更', ['class' => 'btn btn-primary']) !!}
             </div>
             {!! Form::close() !!}
         </div>
