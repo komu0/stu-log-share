c9a5bed40aa3ce7ee73df9cb1d248c81d65ea17f
komu <komu.engineer@gmail.com>
2020-08-20 11:29:26 +0000

    カテゴリ表示順変更のモーダルを修正。

--- resources/views/modal/change_category_order.blade.php
+++ resources/views/modal/change_category_order.blade.php
@@ -1,26 +1,29 @@
-<div class="modal" id="changeCategoryOrder" tabindex="-1" role="dialog" aria-hidden="true">
-     <div class="modal-dialog" role="document">
+<div class="modal" id="changeCategoryOrder" tabindex="-1" aria-hidden="true">
+     <div class="modal-dialog-centered modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">カテゴリ表示順の変更</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <h5 class="modal-title">カテゴリの表示順を変更する</h5>
+                <button type="button" class="close" data-dismiss="modal">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body ml-2">
+            <div class="modal-body container-fluid">
+                <div class="mb-2">
+                    あなたのカテゴリは、様々な場所で、<br>
+                    優先順位の若い順に表示されます。<br>
+                    優先順位は1～9999の数字で指定してください。
+                </div>
+                @foreach ($user->categories as $category)
                 {!! Form::model($category, ['route' => 'update.category.order', 'method' => 'put']) !!}
-                    <p>あなたのカテゴリは、様々な場所で、優先順位の若い順に表示されます。<br>
-                    優先順位は1～9999の数字で指定してください。</p>
-                    @foreach ($user->categories as $category)
-                    <div class="row">
-                        <div class="form-group d-flex align-items-end mr-2">
-                            {!! Form::label("order[$category->id]", $category->name) !!}
-                        </div>
-                        <div class="form-group">
-                            {!! Form::number("order[$category->id]", $category->order, ['class' => 'form-control']) !!}
-                        </div>
+                <div class="row">
+                    <div class="d-flex align-items-end col-sm-3">
+                        {!! Form::label("order[$category->id]", $category->name) !!}
                     </div>
-                    @endforeach
+                    <div class="d-flex align-items-end col-sm-3">
+                        {!! Form::number("order[$category->id]", $category->order, ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+                @endforeach
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">キャンセル</button>
