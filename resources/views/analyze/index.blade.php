defd88cb53c673bb91261c583ad12c9d214aadec
komu <komu.engineer@gmail.com>
2020-08-21 13:20:50 +0000

    分析ページを更新

--- resources/views/analyze/index.blade.php
+++ resources/views/analyze/index.blade.php
@@ -3,13 +3,29 @@
 <h1 class="mb-4">{!! link_to_route('users.show', $user->id, ['user' => $user->id]) !!}さんの分析ページ</h1>
 <section class="mb-3">
     <h2>勉強時間内訳</h2>
-    総勉強時間：{{$user->display_study_time()}}<br>
-    @foreach ($user->categories as $category)
-        <span>　</span>{{$category->name}}：{{ $category->display_study_time() }}<br>
-        @foreach ($category->tags as $tag)
-            <span>　　</span>{{$tag->name}}：{{ $tag->display_study_time() }}<br>
+    <div>
+        <a data-toggle="collapse" href="#collapseCategories" aria-expanded="true">
+        <small>▼</small>
+        </a>
+        総勉強時間：{{$user->display_study_time()}}<br>
+    </div>
+    <div class="collapse show ml-4" id="collapseCategories">
+        @foreach ($user->categories as $i => $category)
+        <div>
+            <a data-toggle="collapse" href="#collapseTags{{$category->id}}" aria-expanded="true">
+            <small>▼</small>
+            </a>
+            {{$category->name}}：{{ $category->display_study_time() }}
+            <div class="collapse show ml-4" id="collapseTags{{$category->id}}">
+                @foreach ($category->tags as $j => $tag)
+                <li>
+                    {{$tag->name}}：{{ $tag->display_study_time() }}
+                </li>
+                @endforeach
+            </div>
+        </div>
         @endforeach
-    @endforeach
+    </div>
 </section>
 <section class="mb-3">
     <h2>勉強時間推移</h2>
