11c6404d2d8ffb30e0d82457f736de2411378b55
komu <komu.engineer@gmail.com>
2020-10-02 05:16:06 +0000

    カテゴリの推移を追加。

--- resources/views/analyze/modal/category_study_time.blade.php
+++ resources/views/analyze/modal/category_study_time.blade.php
@@ -8,14 +8,24 @@
                 </button>
             </div>
             <div class="modal-body ml-3">
-                カテゴリ「{{$category->name}}」の勉強時間は{{$category->study_time()}}時間です。<br>
-                @if ($category->study_time() > 0)
-                    内訳は、<br>
-                    @foreach ($category->tags as $i => $tag)
-                        ・{{$tag->name}}が{{$tag->study_time()}}時間で全体の{{$tag->study_time_percentage()}}%<br>
+                <div class="mb-3">
+                    カテゴリ「{{$category->name}}」の勉強時間は{{$category->study_time()}}時間です。<br>
+                    @if ($category->study_time() > 0)
+                        内訳は、<br>
+                        @foreach ($category->tags as $i => $tag)
+                            ・{{$tag->name}}が{{$tag->study_time()}}時間で全体の{{$tag->study_time_percentage()}}%<br>
+                        @endforeach
+                        です。
+                    @endif
+                </div>
+                <a data-toggle="collapse" href="#collapseAllTrans{{$category->id}}" aria-expanded="true" class="btn btn-primary">
+                    推移
+                </a>
+                <div class="collapse mt-2" id="collapseAllTrans{{$category->id}}">
+                    @foreach ($category->time_trans_array() as $key => $time)
+                        {{$key}}：{{$time}}時間<br>
                     @endforeach
-                    です。
-                @endif
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
