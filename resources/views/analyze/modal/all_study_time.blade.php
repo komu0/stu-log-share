4e7c94b4cf37023e5b72177e2a1b35d0b21f8453
komu <komu.engineer@gmail.com>
2020-10-02 04:06:20 +0000

    総勉強時間の分析を追加。

--- resources/views/analyze/modal/all_study_time.blade.php
+++ resources/views/analyze/modal/all_study_time.blade.php
@@ -2,26 +2,23 @@
      <div class="modal-dialog-centered modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">カテゴリを追加する</h5>
+                <h5 class="modal-title">総勉強時間の分析</h5>
                 <button type="button" class="close" data-dismiss="modal">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body ml-3">
-                {!! Form::model($category, ['route' => 'categories.store']) !!}
-                    <div class="row">
-                        <div class="d-flex align-items-end mr-2">
-                            {!! Form::label('name', 'カテゴリ名:') !!}
-                        </div>
-                        <div>
-                            {!! Form::text('name', null, ['class' => 'form-control']) !!}
-                        </div>
-                    </div>
+                総勉強時間は{{$user->study_time()}}時間です。<br>
+                @if ($user->study_time() > 0)
+                    内訳は、<br>
+                    @foreach ($user->categories as $i => $category)
+                        ・{{$category->name}}が{{$category->study_time()}}時間で全体の{{$category->study_time_percentage()}}%<br>
+                    @endforeach
+                    です。
+                @endif
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
-                {!! Form::submit('追加', ['class' => 'btn btn-primary']) !!}
-                {!! Form::close() !!}
             </div>
         </div>
     </div>
